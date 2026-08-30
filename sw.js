394cd722e52938351586f9fc3dfc54340dfaa757
kawanlamamu <65180789+kawanlamamu@users.noreply.github.com>
2026-08-31 00:45:57 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finansialku-v19';
+const CACHE_NAME = 'finansialku-v18';
 const FILES_TO_CACHE = [
   './',
   './index.html',
@@ -14,25 +14,6 @@ self.addEventListener('install', function(event) {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
-});
-
-// Activate: clean up old caches from previous versions
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(function(key) { return key !== CACHE_NAME; })
-            .map(function(key) { return caches.delete(key); })
-      );
-    })
-  );
-});
-
-// Listen for the "update now" message from the app and activate immediately
-self.addEventListener('message', function(event) {
-  if (event.data === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
 });
 
 // Fetch: serve from cache if offline
